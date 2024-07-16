71c63a74a8f42e2936b40c4cd4a2745fbbc1643b
cmy <1608142614@qq.com>
2024-07-16 13:56:08 +0800

    Finish 876

---
+++ 876. 链表的中间结点.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// 快慢指针的运用
+// 慢一步快两步，保证慢在中间
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
