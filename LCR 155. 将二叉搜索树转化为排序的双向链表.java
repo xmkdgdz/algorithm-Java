29326db38d27728bd514faaa8aa04112a178af83
cmy <1608142614@qq.com>
2024-08-16 19:35:54 +0800

    Finish: LCR155

---
+++ LCR 155. 将二叉搜索树转化为排序的双向链表.java
@@ -0,0 +1,53 @@
+/* 将一个 二叉搜索树 就地转化为一个 已排序的双向循环链表 。
+对于双向循环列表，你可以将左右孩子指针作为双向循环链表的前驱和后继指针，第一个节点的前驱是最后一个节点，最后一个节点的后继是第一个节点。
+特别地，我们希望可以 就地 完成转换操作。当转化完成以后，树中节点的左指针需要指向前驱，树中节点的右指针需要指向后继。还需要返回链表中最小元素的指针。
+ */
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+// 中序遍历，在中序中操作逻辑
+class Solution {
+    Node pre, head;
+
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+
+    void dfs(Node cur){
+        if(cur == null) return;
+        dfs(cur.left);
+        if(pre != null){
+            pre.right = cur;
+            cur.left = pre;
+        }
+        else{
+            head = cur;
+        }
+        pre = cur;
+        dfs(cur.right);
+    }
+}
